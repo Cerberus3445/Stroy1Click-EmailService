0e7bd9319ad8b5350b5c8cf4eb45b3347954052b
Egor <egormalov.2007@gmail.com>
2025-08-21 21:35:46 +0300

    Initial commit

---
+++ src/main/java/ru/stroy1click/email/Stroy1ClickEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.stroy1click.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stroy1ClickEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stroy1ClickEmailServiceApplication.class, args);
+    }
+
+}
